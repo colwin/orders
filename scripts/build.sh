b1bc18f9786448a993f7d8887c71ec34c561c29d
Colwin Fernandes <colwin@opcito.com>
2021-01-14 15:01:44 +0530

    Updated build.sh and docker file for ddagent integration

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,15 +6,15 @@ set -ev
 
 SCRIPT_DIR=$(dirname "$0")
 
-if [[ -z "$GROUP" ]] ; then
-    echo "Cannot find GROUP env var"
-    exit 1
-fi
+# if [[ -z "$GROUP" ]] ; then
+#     echo "Cannot find GROUP env var"
+#     exit 1
+# fi
 
-if [[ -z "$COMMIT" ]] ; then
-    echo "Cannot find COMMIT env var"
-    exit 1
-fi
+# if [[ -z "$COMMIT" ]] ; then
+#     echo "Cannot find COMMIT env var"
+#     exit 1
+# fi
 
 if [[ "$(uname)" == "Darwin" ]]; then
     DOCKER_CMD=docker
@@ -25,8 +25,9 @@ CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 $DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
 
-cp -r $CODE_DIR/docker $CODE_DIR/target/docker/
-cp -r $CODE_DIR/target/*.jar $CODE_DIR/target/docker/${IMAGE}
+#cp -r $CODE_DIR/docker $CODE_DIR/target/docker/
+#cp -r $CODE_DIR/target/*.jar $CODE_DIR/target/docker/${IMAGE}
+cp -r $CODE_DIR/target/*.jar $CODE_DIR/docker/orders
 
-REPO=${GROUP}/${IMAGE}
-    $DOCKER_CMD build -t ${REPO}:${COMMIT} $CODE_DIR/target/docker/${IMAGE};
+# REPO=${GROUP}/${IMAGE}
+#     $DOCKER_CMD build -t ${REPO}:${COMMIT} $CODE_DIR/target/docker/${IMAGE};
